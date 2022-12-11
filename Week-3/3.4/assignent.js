cd752a6d079a36ecca0585f4ecc51d5b9e59980a
ben k shaji <benkshaji20@gmail.com>
2022-12-12 00:50:47 +0530

    week3 assignment done

--- Week-3/3.4/assignent.js
+++ Week-3/3.4/assignent.js
@@ -1,22 +1,29 @@
-function createIncrement() {
-    let count = 0;
+function createStack() {
+  let items = [];
+  return {
+      push(item) {
+          return items.push(item);
+      },
+      pop() {
+          return items.pop();
+      },
+      print(){
+          return console.log('items :>> ', items);
+      }
+  };
+}
+const stack = createStack();
+stack.print();
+stack.push(10);
+stack.print();
+stack.push(5);
+stack.print();
+stack.pop();
+stack.print();
 
-    function increment () {
-      count++;
-    }
+console.log('stack.items :>> ', stack.items);
+stack.items = [1,2,3];
+stack.print();
 
-    let message = `Count is ${count}`;
-
-    function log() {
-      console.log(message);
-    }
-
-    return [increment, log];
-  }
-
-  const [increment, log] = createIncrement();
-
-  increment(); // These function calls increases count three times
-  increment();
-  increment();
-  log(); // log functions doesn't print the updated count . Since we are initializing message at the time of createIncrement function call. Message doesn't get updated at the time of increment function call. Hence we get initial value of count;
+// In the previos implementation we were sending items as a property of returned object, hence items array was muttable.
+//  By moving items array to its parent, we are able to only access or update it through methods of the returned object but not able to access it directly from outside hence made it immuttable
